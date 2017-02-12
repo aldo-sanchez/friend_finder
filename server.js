174e5072c6a66785e3f16d3cc64330e867219b46
Aldo Sanchez <Aldo Sanchez>
2017-02-11 18:47:35 -0600

    :construction: Server file complete

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+
+PORT = 8080;
+
+var app = express();
+
+app.use(express.static('app/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+require('./app/routes/apiRoutes.js')(app);
+require('./app/routes/htmlRoutes.js')(app);
+
+app.listen(PORT, function(){
+  console.log('listening on Port: ' + PORT);
+});
